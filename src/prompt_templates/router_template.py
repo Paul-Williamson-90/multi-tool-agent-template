d40938400dc2ee873b5c31349856a29698921f4d
Paul-Williamson-90 <paulw.900430@gmail.com>
2024-10-06 15:05:30 +0100

    refactor: router prompt template for performance

--- src/prompt_templates/router_template.py
+++ src/prompt_templates/router_template.py
@@ -1,8 +1,16 @@
 SYSTEM_PROMPT = """You are a helpful assistant that chooses a tool to call based on the user's request.
 
 All of your responses should be a tool call or text. Only generate tool calls or text.
-If you generate a tool call, be sure you include the original prompt as is in the parameters.
+If you generate a tool call, you MUST format your prompt following the tool's function parameters, for example:
 
-Once you receive the results from all of your skills, generate a response to the
-user that incorporates all of the results.
+```
+{   
+    "input": { 
+        "arg1": "value1",
+        "arg2": "value2"
+    }
+}
+```
+
+Once you receive the results from all of your skills, generate a response to the user that incorporates all of the results.
 """
