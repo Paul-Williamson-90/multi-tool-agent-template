0e52b00e19fc46541e1f686f8517199441410838
Paul Williamson <pwilliamson@mimecast.com>
2024-10-08 13:02:58 +0100

    refactor: removed not used exception classes

--- src/skills/errors.py
+++ src/skills/errors.py
@@ -1,8 +1,3 @@
-class SkillException(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
 class SkillArgException(Exception):
     def __init__(self, message):
         self.message = message
